ca1c5d5707602aaabf5f5399afe24a6f44611f04
HebertLemuel <Hebertestudos97@gmail.com>
2025-10-24 09:33:28 -0300

    menu add

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-import json
-import professor
-import aluno 
-
+import json 
+from aluno import Aluno
+from professor import Professor
 
 class Menu:
-    def __init__(self, nome, cargo, senha):
-        self.nome = nome
-        self.cargo = cargo
-        self.__senha = senha
+    def __init__(self):
+        self.arquivo_dados = 'dados.json' # atributo  que guarda o nome do arquivo onde os dados serão lidos
+        self.carregar_dados(self) # chama o metodo carregar dados para iniciar o menu
 
-    def menu(self):
-        self.nome = input("Digite o Nome do Usuario")
-        self.cargo = input("Digite o cargo, ex('Professor, Aluno')")
-        self.__senha = int(input("Digite a senha"))
-        lista = [self.nome, self.cargo,]
+    def carregar_dados(self):
+        # Aqui você pode adicionar a lógica para converter os dados carregados em objetos Aluno e Professor
+        try:
+            with open(self.arquivo_dados, "r", encoding="UTF-8") as arquivos: # ler e criar a variavel "arquivos"
+                self.dados = json.load(arquivos) # parsear o conteúdo JSON do arquivo e transforma em um dicionário Python
+        except FileNotFoundError: # para error na digitação do nome do arquivo
+            print("Arquivo de dados não encontrado. ") # Inicia com dados vazios ou cria um novo arquivo, se necessário]
+            
+        opcao = input("Deseja criar um novo arquivo de dados? (s/n): ") # Pergunta ao usuário se deseja criar um novo arquivo de dados
+        if opcao =="s":
+            self.dados = {}
+            print("Novo arquivo de dados criado.")
+        else:
+            print("Continuando sem criar um novo arquivo.")
+            exit()
 
-    def lista(self, lista):
-        print(lista)
-
-
-if __name__ == "__main__":
-    Menu()
+        def salvar_dados(self):
+            with open(self.arquivo_dados, "w", encoding="UTF-8") as arquivos: # abrir o arquivo em modo de escrita
+                json.dump(self.dados, arquivos, indent=4, ensure_ascii = False) # salvar os dados no arquivo em formato JSON
