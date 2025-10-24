1a125b0167037851071c7a05dfc6d09d65f46fc2
HebertLemuel <Hebertestudos97@gmail.com>
2025-10-24 07:56:25 -0300

    Add files via upload

---
+++ disciplina.py
@@ -0,0 +1,4 @@
+class Disciplina:
+    def __init__(self, nome, carga_horaria):
+        self.nome = nome
+        self.carga_horaria = carga_horaria
---
+++ nota.py
@@ -0,0 +1,4 @@
+class Nota:
+    def __init__(self, disciplina, valor):
+        self.disciplina = disciplina
+        self.valor = valor
