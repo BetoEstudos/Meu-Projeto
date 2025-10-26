44c6a2f3d944ab4982fcd812d34a7187520591a8
HebertLemuel <Hebertestudos97@gmail.com>
2025-10-26 12:07:26 -0300

    add disciplina

--- disciplina.py
+++ disciplina.py
@@ -1,4 +1,9 @@
 class Disciplina:
     def __init__(self, nome, carga_horaria):
         self.nome = nome
+        self.lista_alunos = []
         self.carga_horaria = carga_horaria
+        
+    def add_aluno(self, aluno):
+        if aluno not in self.lista_alunos:
+            self.lista_alunos.append(aluno)
